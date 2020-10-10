5e015cf4c72498749be9bb4a4b989f154c38f799
VincentSastra <vincent.sastra@gmail.com>
2020-10-09 20:11:50 -0700

    Added simple jumping

---
+++ Assets/characterMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public bool isGrounded;
+    public float jumpForce = 2.0f;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void onCollisionStay() {
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rb.velocity = new Vector2(rb.velocity.x, 10f);
+            isGrounded = false;
+        }
+    }
+}
