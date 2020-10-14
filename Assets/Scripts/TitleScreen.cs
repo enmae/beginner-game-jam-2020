67507560bacb3d31441543047558dac3194c4102
enmae <mmaring@bell.net>
2020-10-14 10:23:51 -0400

    Reworked some of the code for pausing so it can toggle the pause menu

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -3,26 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TitleScreen : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start() {
-    }
+public class TitleScreen : MonoBehaviour {
+    public GameObject pauseMenuUI;
+    public GameObject optionMenuUI;
+
+    public static bool gameIsPaused = false;
 
     // Update is called once per frame
     void Update() {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (gameIsPaused) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
     }
 
     public void Play() {
         SceneManager.LoadScene(1);
     }
 
-    public void Resume() {
-        // to make canvas visible
+    void Pause() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
     }
 
-    public void Options() {
+    public void Resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Options() {
         // secondary canvas for different options
     }
 }
