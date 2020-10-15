b263e32cbfe09b576d6e93e1b54d92e15cf51499
enmae <mmaring@bell.net>
2020-10-15 17:39:22 -0400

    Finished reworking the pause screen for the most part (it works, though there are still some things to tweak). The options menu is not finished. Added a new scene for testing the pause screen so it would not clash with Moses' game scene. Deleted my version of the game scene to make merging easy

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -8,35 +8,53 @@ public class TitleScreen : MonoBehaviour {
     public GameObject optionMenuUI;
 
     public static bool gameIsPaused = false;
+    public static bool optionsAreShowing = false;
+    public static bool pauseIsHidden = false;
 
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.Escape)) {
-            if (gameIsPaused) {
+            if (gameIsPaused && !optionsAreShowing) {
                 Resume();
+            } else if (gameIsPaused && optionsAreShowing) {
+                LoadOptions();
+            } else if (gameIsPaused && pauseIsHidden && !optionsAreShowing) {
+                QuitOptions();
             } else {
                 Pause();
             }
         }
     }
 
-    public void Play() {
-        SceneManager.LoadScene(1);
-    }
-
     void Pause() {
         pauseMenuUI.SetActive(true);
+        optionMenuUI.SetActive(false);
         Time.timeScale = 0f;
         gameIsPaused = true;
     }
 
     public void Resume() {
         pauseMenuUI.SetActive(false);
+        optionMenuUI.SetActive(false);
         Time.timeScale = 1f;
         gameIsPaused = false;
+        optionsAreShowing = false;
     }
 
-    void Options() {
+    public void LoadOptions() {
         // secondary canvas for different options
+        optionMenuUI.SetActive(true);
+        optionsAreShowing = true;
+        pauseIsHidden = true;
+        pauseMenuUI.SetActive(false);
+        Debug.Log("Loading options...");
+    }
+
+    public void QuitOptions() {
+        Debug.Log("Quitting options...");
+        optionsAreShowing = false;
+        pauseIsHidden = false;
+        optionMenuUI.SetActive(false);
+        pauseMenuUI.SetActive(true);
     }
 }
